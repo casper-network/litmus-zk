18709fb9b3994e4f6ae1599dafd37735585a95d4
Mark A. Greenslade <asladeofgreen@gmail.com>
2025-01-10 13:08:01 +0100

    1. Updated sp1-elf build.

---
+++ zk-pgms/sp1/build.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+function _help() {
+    echo "
+    DESCRIPTION
+    ----------------------------------------------------------------
+    Compiles litmus program for execution within SP1 ZK-VM.
+
+    NOTES
+    ----------------------------------------------------------------
+    Assumes that sp1 toolchain is installed and is available at '$HOME/.sp1'.
+    "
+}
+
+function _main()
+{
+    # Activate sp1 toolchain.
+    export PATH="$PATH:$HOME/.sp1/bin"
+
+    # Build elf.
+    cargo prove build \
+        --elf-name litmus-riscv32im-zkvm-sp1-elf \
+        --output-directory zk-pgms/sp1/elf
+}
+
+# ----------------------------------------------------------------
+# ENTRY POINT
+# ----------------------------------------------------------------
+
+unset _HELP
+
+for ARGUMENT in "$@"
+do
+    KEY=$(echo "$ARGUMENT" | cut -f1 -d=)
+    VALUE=$(echo "$ARGUMENT" | cut -f2 -d=)
+    case "$KEY" in
+        help) _HELP="show" ;;
+        *)
+    esac
+done
+
+if [ "${_HELP:-""}" = "show" ]; then
+    _help
+else
+    _main
+fi
